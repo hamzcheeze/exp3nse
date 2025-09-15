b8fb14e34f37b59f3586ea65c197938c46518c07
HaMzCheeZe <ham_anurak@hotmail.com>
2025-09-15 18:25:12 +0700

    ignore .env

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -71,7 +71,7 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <p className="font-bold text-inherit">Expense</p>
+            <p className="font-bold text-inherit">App Name</p>
           </NextLink>
         </NavbarBrand>
       </NavbarContent>
